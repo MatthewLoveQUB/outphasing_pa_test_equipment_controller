bfd7a87724412c154537912ef29aa98c39a1aacb
MatthewLoveQUB <mlove05@qub.ac.uk>
2020-01-29 11:48:31 +0000

    added non-working UI element for defining search

---
+++ OutphasingSweepController/PhaseSearchSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutphasingSweepController
+    {
+    class PhaseSearchSettings
+        {
+        List<PhaseSearchSingleSetting> Settings;
+
+        public PhaseSearchSettings(string settingsInput)
+            {
+            this.Settings = new List<PhaseSearchSingleSetting>();
+            var splitInputs = settingsInput
+                .Split(';')
+                .Where(s => s != "")
+                .ToList();
+            foreach(var input in splitInputs)
+                {
+                var splitSetting = input
+                    .Split(',')
+                    .Select(x => Convert.ToDouble(x))
+                    .ToList();
+                var newSetting = new PhaseSearchSingleSetting(
+                    splitSetting[0], splitSetting[1]);
+                this.Settings.Add(newSetting);
+                }
+            }
+        }
+    }
---
+++ OutphasingSweepController/PhaseSearchSingleSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutphasingSweepController
+    {
+    class PhaseSearchSingleSetting
+        {
+        public double StepDeg;
+        public double ThresholddB;
+        public PhaseSearchSingleSetting(double stepDeg, double thresholddB)
+            {
+            this.StepDeg = stepDeg;
+            this.ThresholddB = thresholddB;
+            }
+        }
+    }
